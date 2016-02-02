956201c810114ba868fb7ebb53f8186821b51ea8
Brian Keller-Heikkila <brian@keller-heikkila.org>
2016-02-02 17:43:10 +0000

    Add CORS response headers

--- RrrHello/__init__.py
+++ RrrHello/__init__.py
@@ -8,6 +8,14 @@ app.config.from_pyfile('../config.py')
 api = Api(app)
 api.add_resource(HelloAPI, '/hello', '/hello/<string:name>')
 
+@app.after_request
+def after_request(response):
+  response.headers.add('Access-Control-Allow-Origin', '*')
+  response.headers.add('Access-Control-Allow-Headers', 'Content-Type')
+  response.headers.add('Access-Control-Allow-Methods', 'OPTIONS,GET')
+  return response
+
+
 if __name__ == '__main__':
     app.run(debug=True)
 
